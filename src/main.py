f397433011561d6dc89556263e67b5f6cf99c240
Marcos Bellezi <mbellezi@gmail.com>
2020-07-07 18:07:31 -0300

    feature: restructured app and loaded models
    Restructured the application as a class.
    Loaded all the models.

--- src/main.py
+++ src/main.py
@@ -1,58 +1,90 @@
 import cv2
+import logging
 from argparse import ArgumentParser
 from input_feeder import InputFeeder
+from model_face_detection import Model_Face_Detection
+from model_facial_landmark_detection import Model_Facial_Landmark_Detection
+from model_gaze_estimation import Model_Gaze_Estimation
+from model_head_pose_estimation import Model_Head_Pose_estimation
+import logging as log
 
 
-def build_argparser():
-    """
-    Parse command line arguments.
+class Application:
 
-    :return: command line arguments
-    """
-    parser = ArgumentParser()
-    parser.add_argument("-t", "--input-type", required=True, type=str,
-                        help="Type of input (video, image or cam)")
-    parser.add_argument("-i", "--input", required=True, type=str,
-                        help="Input file")
-    parser.add_argument("-o", "--out", type=str, default=None,
-                        help="Output file with the processed content")
+    def __init__(self):
+        self.args = None
+        self.feed = None
+        self.face_detection_model = None
+        self.facial_landmark_detection_model = None
+        self.gaze_estimation_model = None
+        self.head_pose_estimation_model = None
 
-    return parser
+    def initialize_argparser(self):
+        """
+        Parse command line arguments.
 
+        :return: command line arguments
+        """
+        parser = ArgumentParser()
+        parser.add_argument("-t", "--input-type", required=True, type=str,
+                            help="Type of input (video, image or cam)")
+        parser.add_argument("-i", "--input", required=True, type=str,
+                            help="Input file")
+        parser.add_argument("-o", "--out", type=str, default=None,
+                            help="Output file with the processed content")
+        self.args = parser.parse_args()
 
-def initialize_feed(input_type, input):
-    feed = InputFeeder(input_type, input)
-    feed.load_data()
-    return feed
+    def initialize_logging(self):
+        log.basicConfig(level=log.DEBUG)
 
+    def initialize_feed(self):
+        self.feed = InputFeeder(self.args.input_type, self.args.input)
+        self.feed.load_data()
 
-def initialize_window():
-    cv2.namedWindow('preview')
+    def initialize_window(self):
+        cv2.namedWindow('preview')
 
+    def infer_frame(self, frame):
+        cv2.imshow('preview', frame)
+        key_pressed = cv2.waitKey(1)
+        if key_pressed == 27:
+            return False
 
-def infer_frame(frame, feed_type):
-    cv2.imshow('preview', frame)
-    key_pressed = cv2.waitKey(1)
-    if key_pressed == 27:
-        return False
-
-
-def process_feed(feed, feed_type):
-    for batch in feed.next_batch():
-        if batch is not False:
-            if infer_frame(batch, feed_type) is False:
+    def process_feed(self):
+        for batch in self.feed.next_batch():
+            if batch is not False:
+                if self.infer_frame(batch) is False:
+                    return
+            else:
                 return
-        else:
-            return
 
+    def initialize_models(self):
+        self.face_detection_model = Model_Face_Detection(
+            "models/intel/face-detection-adas-binary-0001/FP32-INT1/face-detection-adas-binary-0001")
+        self.face_detection_model.load_model()
 
-def main():
-    args = build_argparser().parse_args()
-    feed = initialize_feed(args.input_type, args.input)
-    initialize_window()
-    process_feed(feed, args.input_type)
-    feed.close()
+        self.facial_landmark_detection_model = Model_Facial_Landmark_Detection(
+            "models/intel/landmarks-regression-retail-0009/FP32/landmarks-regression-retail-0009")
+        self.facial_landmark_detection_model.load_model()
+
+        self.gaze_estimation_model = Model_Gaze_Estimation(
+            "models/intel/gaze-estimation-adas-0002/FP32/gaze-estimation-adas-0002")
+        self.gaze_estimation_model.load_model()
+
+        self.head_pose_estimation_model = Model_Head_Pose_estimation(
+            "models/intel/head-pose-estimation-adas-0001/FP32/head-pose-estimation-adas-0001")
+        self.head_pose_estimation_model.load_model()
+
+    def run(self):
+        self.initialize_logging()
+        self.initialize_argparser()
+        self.initialize_models()
+        self.initialize_feed()
+        self.initialize_window()
+        self.process_feed()
+        self.feed.close()
 
 
 if __name__ == '__main__':
-    main()
+    app = Application()
+    app.run()
--- src/model_gaze_estimation.py
+++ src/model_gaze_estimation.py
@@ -1,35 +1,107 @@
+import os
+import logging as log
+from openvino.inference_engine import IENetwork, IECore
+
 '''
 This is a sample class for a model. You may choose to use it as-is or make any changes to it.
 This has been provided just to give you an idea of how to structure your model class.
 '''
 
-class Model_X:
+
+class Model_Gaze_Estimation:
     '''
     Class for the Face Detection Model.
     '''
+
     def __init__(self, model_name, device='CPU', extensions=None):
         '''
         TODO: Use this to set your instance variables.
         '''
-        raise NotImplementedError
+        self.model_name = model_name
+        self.device = device
+        self.extensions = extensions
+        self.ie = None
+        self.network = None
+        self.exec_network = None
+        self.infer_request = None
+        self.input_name = None
+        self.input_shape = None
+        self.output_name = None
+        self.output_shape = None
+        self.model_width = None
+        self.model_height = None
 
     def load_model(self):
         '''
-        TODO: You will need to complete this method.
-        This method is for loading the model to the device specified by the user.
-        If your model requires any Plugins, this is where you can load them.
+        Load the model given IR files.
+        Defaults to CPU as device for use in the workspace.
+        Synchronous requests made within.
         '''
-        raise NotImplementedError
+
+        log.info(f"##### Loading Model: {self.model_name}")
+        model_xml = self.model_name + ".xml"
+        model_bin = self.model_name + ".bin"
+
+        # Initialize the inference engine
+        self.ie = IECore()
+
+        # Add a CPU extension, if applicable
+        if self.extensions and "CPU" in self.device:
+            self.ie.add_extension(self.extensions, self.device)
+
+        # Read the IR as a IENetwork
+        self.network = self.ie.read_network(model=model_xml, weights=model_bin)
+
+        self.check_model()
+
+        # Load the IENetwork into the inference engine
+        self.exec_network = self.ie.load_network(self.network, self.device)
+
+        # Get the layer's info
+        self.input_name = next(iter(self.exec_network.inputs))
+        self.output_name = next(iter(self.exec_network.outputs))
+        self.input_shape = self.exec_network.inputs[self.input_name].shape
+        self.output_shape = self.exec_network.outputs[self.output_name].shape
+
+        log.info(f"Input shape: {self.input_shape}")
+        log.info(f"Output shape: {self.output_shape}")
+
+        return self.exec_network
+
+    def get_input_shape(self):
+        '''
+        Gets the input shape of the network
+        '''
+        return self.input_shape
+
+    def get_output_shape(self):
+        '''
+        Gets the output shape of the network
+        '''
+        return self.output_shape
 
     def predict(self, image):
         '''
-        TODO: You will need to complete this method.
         This method is meant for running predictions on the input image.
         '''
-        raise NotImplementedError
+        outputs = []
+        frame_inference = self.preprocess_input(image)
+
+        # Start asynchronous inference for specified request
+        self.exec_network.start_async(request_id=0, inputs={self.input_name: frame_inference})
+        if self.exec_network.requests[0].wait(-1) == 0:
+            outputs = self.preprocess_output(self.exec_network.requests[0].outputs[self.output_name])
+        return outputs
 
     def check_model(self):
-        raise NotImplementedError
+        ### Check for any unsupported layers, and let the user
+        ### know if anything is missing. Exit the program, if so.
+        supported_layers = self.ie.query_network(network=self.network, device_name=self.device)
+        unsupported_layers = [l for l in self.network.layers.keys() if l not in supported_layers]
+        if len(unsupported_layers) != 0:
+            log.error("Unsupported layers found: {}".format(unsupported_layers))
+            log.error("Check whether extensions are available to add to IECore.")
+            exit(1)
 
     def preprocess_input(self, image):
         '''
